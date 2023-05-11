f2f843a0c724e5466e1cafbf726b1974f19599b8
qa-nbgel-0-llyx@example.com <qa-nbgel-0-llyx@example.com>
2023-05-11 14:11:31 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16444.stoplight-dev.com/api/v1/projects/cHJqOjc1Nw/spectral.js?branch_slug=main&token=9e644f76-407c-405e-9e47-a46aebaf14f9";
+export default { extends: ruleset };
